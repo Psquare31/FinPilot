1f68a5e346d45bc1f3a54164b2100433e404bf53
mayank-nishant <mail.nishantmayank@gmail.com>
2026-07-06 10:47:02 +0530

    chore(server): initialize backend application layer

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,11 +1,12 @@
 export { default as User } from "./User.js";
+export { default as RefreshToken } from "./RefreshToken.js";
 export { default as Workspace } from "./Workspace.js";
 export { default as WorkspaceMember } from "./WorkspaceMember.js";
-export { default as RefreshToken } from "./RefreshToken.js";
+export { default as Invitation } from "./Invitation.js";
 
-export { default as Category } from "./Category.js";
 export { default as Account } from "./Account.js";
 export { default as Transaction } from "./Transaction.js";
+export { default as Category } from "./Category.js";
 export { default as Budget } from "./Budget.js";
 export { default as Goal } from "./Goal.js";
 
@@ -14,9 +15,7 @@ export { default as InvestmentTransaction } from "./InvestmentTransaction.js";
 export { default as Debt } from "./Debt.js";
 
 export { default as Notification } from "./Notification.js";
-export { default as Invitation } from "./Invitation.js";
 export { default as Report } from "./Report.js";
 export { default as AuditLog } from "./AuditLog.js";
 export { default as AiInteraction } from "./AiInteraction.js";
-
 export { default as Subscription } from "./Subscription.js";
